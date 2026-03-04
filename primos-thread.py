2d9fb2b37f3f2dff03a9563a420a1d29d0d105b9
Andrei Alves Fracalossi <124471216+andreihmm@users.noreply.github.com>
2026-03-04 18:20:33 -0300

    Update primos-thread.py

--- primos-thread.py
+++ primos-thread.py
@@ -1,41 +1,47 @@
-from my_thread import MyThread
-import time
-from math import sqrt
 import threading
+from math import sqrt
 
+def verificarPrimos(inicio, fim, resultados):
+    for numero in range(inicio, fim):
+        if numero < 2:
+            continue
+        
+        if numero == 2:
+            resultados.append(numero)
+            continue
+        
+        if numero % 2 == 0:
+            continue
 
-def verificarPrimo(numero):
-    if numero < 2:
-        print(f"Número {numero} não é primo")
-        return False
-    
-    if numero == 2:
-        print(f"Número {numero} É primo")
-        return numero
-    
-    if numero % 2 == 0:
-        print(f"Número {numero} não é primo")
-        return False
-
-    limit = int(sqrt(numero)) + 1
-    for i in range (3, limit, 2):
-        if numero % i == 0:
-            print(f"Número {numero} não é primo")
-            return False
-    
-    print(f"Número {numero} É primo")
-    return numero 
+        for i in range(3, int(sqrt(numero)) + 1, 2):
+            if numero % i == 0:
+                break
+        else:
+            resultados.append(numero)
 
 
 def calcularPrimos():
+    resultados = []
     threads = []
-    for i in range (10, 21):
-        t = threading.Thread(target=verificarPrimo, args=(i,))
-        t.start()
-        threads.append(t)
 
-    for thread in threads:
-        thread.join()
+    n_threads = 4
+    intervalo = 50000
+    inicio_base = 10**6
+    chunk = intervalo // n_threads
+
+    for i in range(n_threads):
+        inicio = inicio_base + i * chunk
+        fim = inicio_base + (i + 1) * chunk
+
+        t = threading.Thread(target=verificarPrimos, args=(inicio, fim, resultados))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    print("Total de primos:", len(resultados))
+
 
 if __name__ == "__main__":
     calcularPrimos()
